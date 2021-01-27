3d558428e39abef08ab4adf584fb664fe69b0aeb
jean-zhou0512 <2434153436@qq.com>
2021-01-28 00:19:19 +0800

    添加字典表接口查询

--- service/service_edu/src/main/java/com/cn/edu/controller/SysDictController.java
+++ service/service_edu/src/main/java/com/cn/edu/controller/SysDictController.java
@@ -1,9 +1,17 @@
 package com.cn.edu.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.cn.edu.entity.SysDict;
+import com.cn.edu.service.ISysDictService;
+import com.cn.utils.basics.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -13,8 +21,24 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zjm
  * @since 2021-01-26
  */
+@Api(description = "字典值管理")
 @RestController
-@RequestMapping("/edu/sys-dict")
+@RequestMapping("/eduservice/dict/")
+@CrossOrigin() //解决跨域注解
 public class SysDictController {
+
+    @Autowired
+    private ISysDictService sysDictService;
+
+    @ApiOperation(value = "根据传入的字典类型获取字典列表")
+    @GetMapping(value="qryDictListByType/{type}")
+    public Result qryDictListByType(
+            @ApiParam(name = "type",value = "字典类型",required = true)
+            @PathVariable String type){
+        QueryWrapper<SysDict> dictQueryWrapper = new QueryWrapper<>();
+        dictQueryWrapper.eq("dict_type",type);
+        List<SysDict> dictList = sysDictService.list(dictQueryWrapper);
+        return Result.ok().data("dictList",dictList);
+    }
 
 }
